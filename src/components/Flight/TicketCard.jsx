ed7da0c81d968552228f6b44b3e9484bfef186ab
mariam <mariam.nasser961@gmail.com>
2025-09-13 17:55:43 +0300

    links

--- src/components/Flight/TicketCard.jsx
+++ src/components/Flight/TicketCard.jsx
@@ -1,36 +1,39 @@
 import React from "react";
 import plane from "../../assets/icons/plane.png";
 
-function TicketCard() {
+function TicketCard({ click }) {
   return (
-    <div className="flex justify-center" >
-    <div className=" flex flex-col w-[608px] gap-4 px-8 py-4 h-[182px] rounded-2xl bg-white shadow-[0px_2px_8px_0px_#838BB41F]">
-      {/* first part */}
-      <div className="flex justify-between">
-        <div>
-          <h1 className="text-[#111928]">7:05 AM</h1>
-          <h1 className="text-[#6B7280]">YUL</h1>
+    <div className="flex justify-center ">
+      <div
+        className=" flex flex-col  w-[608px] gap-4 px-8 py-4 h-[150px] rounded-2xl bg-white shadow-[0px_2px_8px_0px_#838BB41F] cursor-pointer hover:bg-gray-100"
+        onClick={click}
+      >
+        {/* first part */}
+        <div className="flex justify-between">
+          <div>
+            <h1 className="text-[#111928]">7:05 AM</h1>
+            <h1 className="text-[#6B7280]">YUL</h1>
+          </div>
+          <div className="text-[#111928] flex flex-col items-center ">
+            <img src={plane} />
+            <h1>18:55</h1>
+          </div>
+          <div>
+            <h1 className="text-[#111928]">8:55 PM</h1>
+            <h1 className="text-[#6B7280]">YUL</h1>
+          </div>
         </div>
-        <div className="text-[#111928] flex flex-col items-center ">
-          <img src={plane} />
-          <h1>18:55</h1>
-        </div>
-        <div>
-          <h1 className="text-[#111928]">8:55 PM</h1>
-          <h1 className="text-[#6B7280]">YUL</h1>
+        {/* second part */}
+        <div className="flex justify-between">
+          <div>
+            <h1 className="text-[#6B7280]">1 layover: YYZ (3:55)</h1>
+            <h1 className="text-gray-900">Scoot</h1>
+          </div>
+          <div>
+            <h1 className="#111928">$ 1,300</h1>
+          </div>
         </div>
       </div>
-      {/* second part */}
-      <div className="flex justify-between">
-        <div>
-          <h1 className="text-[#6B7280]">1 layover: YYZ (3:55)</h1>
-          <h1 className="text-gray-900">Scoot</h1>
-        </div>
-        <div>
-          <h1 className="#111928">$ 1,300</h1>
-        </div>
-      </div>
-    </div>
     </div>
   );
 }
--- src/pages/Flight/TicketsPage.jsx
+++ src/pages/Flight/TicketsPage.jsx
@@ -9,28 +9,22 @@ import planemobile from "../../assets/images/planemobile.jpg";
 import { useNavigate } from "react-router";
 
 function TicketsPage() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
     <div className=" flex flex-col mx-auto max-w-[1240px]  md:py-8  ">
-
       <div className="hidden md:block ml-[100px] mb-1">
         <BackButton />
       </div>
-           {/* mobile */}
-          <div className=" relative w-full  md:hidden">
-            <img className="object-cover static " src={planemobile} alt="" />
-            <div className=" flex items-center gap-[70px] absolute top-5 left-4" >
-              <button onClick={() => navigate(-1)} >
-              <img
-                src={back}
-                className="size-6 md:hidden cursor-pointer "
-              />
-              </button  >
-              <h1 className=" text-white text-lg ">
-                Select Your Flight
-              </h1>
-            </div>
-          </div>
+      {/* mobile */}
+      <div className=" relative w-full  md:hidden">
+        <img className="object-cover static " src={planemobile} alt="" />
+        <div className=" flex items-center gap-[70px] absolute top-5 left-4">
+          <button onClick={() => navigate(-1)}>
+            <img src={back} className="size-6 md:hidden cursor-pointer " />
+          </button>
+          <h1 className=" text-white text-lg ">Select Your Flight</h1>
+        </div>
+      </div>
       <div className="grid gap-14 p-3 mx-1.5 lg:mx-24 grid-cols-1 lg:grid-cols-2">
         {/* column 1 */}
         <div className="flex flex-col gap-6 ">
@@ -38,20 +32,20 @@ function TicketsPage() {
             <img src={calender}></img>
             <h1 className="text-secondary-900 text-xl">Dec 16th, 2025</h1>
           </div>
-          <TicketCard />
-          <TicketCard />
-          <TicketCard />
+          <TicketCard click={() => navigate("/seat")} />
+          <TicketCard click={() => navigate("/seat")} />
+          <TicketCard click={() => navigate("/seat")} />
         </div>
 
         {/* column 2 */}
         <div className="flex flex-col gap-6">
           <div className="gap-2 h-[80px] bg-white  shadow-[0px_2px_8px_0px_#838BB41F] rounded-xl flex items-center justify-center text-lg font-medium">
-            <img  src={vector}></img>
+            <img src={vector}></img>
             <h1 className="text-[#111928] text-xl">Jan 6th,2025</h1>
           </div>
-          <TicketCard />
-          <TicketCard />
-          <TicketCard />
+          <TicketCard click={() => navigate("/seat")} />
+          <TicketCard click={() => navigate("/seat")} />
+          <TicketCard click={() => navigate("/seat")} />
         </div>
       </div>
     </div>
